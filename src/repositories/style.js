2d515c841e992a9447b664be4d7b23684c17726f
Stephany3pg <stephany3pg@gmail.com>
2022-12-09 16:13:47 -0300

    initial commit

---
+++ src/repositories/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Repositories = styled.View``;
+// width: 70px;
+// height: 70px;
+// backgroundColor: #E15B42;
+// border-radius: 35px;
+// transform: scaleX(1.2);
+export default Repositories;

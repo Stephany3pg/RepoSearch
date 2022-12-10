ba1edaa9eb965a86f21d098e0887ccb7fc213438
Stephany3pg <stephany3pg@gmail.com>
2022-12-10 14:09:58 -0300

    Reorganize file and component names

--- src/components/search/style.js
+++ src/components/search/style.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
-const Home = styled.View``;
+const Search = styled.View``;
 // width: 70px;
 // height: 70px;
 // backgroundColor: #E15B42;
 // border-radius: 35px;
 // transform: scaleX(1.2);
-export default Home;
+export default Search;

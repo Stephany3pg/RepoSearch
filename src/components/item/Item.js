798162a2d6377a6b5034b34ebe15ac395524b871
Stephany3pg <stephany3pg@gmail.com>
2022-12-10 21:57:42 -0300

    Adjust error

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import { ListItem, Avatar } from 'react-native-elements';
-import styled from 'styled-components';
+import { Subtitles, Title, Row } from './style';
 
 const Item = ({ data }) => {
-  const Row = styled.View`
-    flex-direction: row;
-    width: 100%;
-    justify-content: space-between;
 
-  `;
-  const Subtitles = styled.Text`
-    color: gray;
-  `;
-  const Title = styled.Text`
-    font-weight: bold;
-  `;
-  
   const stars = data.stars > 1 ? 'stars' : 'star';
   return (
     <ListItem key={data.id} bottomDivider>
-        <Avatar rounded source={require('../../assets/image.jpeg')} ></Avatar>//TODO: passa url da imagem pelo data
+        <Avatar rounded source={require('../../assets/image.jpeg')} ></Avatar>
         <ListItem.Content>
             <ListItem.Title>
                 <Row>
                     <Title>{data.repo}</Title>
-                    <Subtitles>{data.stars} {stars}</Subtitles>
+                    <Subtitles>{`${data.stars} ${stars}`}</Subtitles>
                 </Row>
             </ListItem.Title>
             <ListItem.Subtitle><Subtitles>{data.user}</Subtitles></ListItem.Subtitle>
---
+++ src/components/item/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Row = styled.View`
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-between;
+`;
+
+export const Subtitles = styled.Text`
+    color: gray;
+`;
+
+export const Title = styled.Text`
+    font-weight: bold;
+`;

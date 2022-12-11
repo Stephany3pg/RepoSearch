4a7f3b16f188e3a989e15b55ac21e9ee5781c60e
Stephany3pg <stephany3pg@gmail.com>
2022-12-11 11:28:18 -0300

    Search dispatch

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { SearchBar } from 'react-native-elements';
+import { useDispatch } from 'react-redux';
+import { searchRepositories } from '../../redux/actions/repo';
 
 const Search = () => {
     const [search, setSearch] = useState("");
@@ -7,11 +9,16 @@ const Search = () => {
         setSearch(text);
     };
 
+    const dispatch = useDispatch();
+    const fetchRepositories = () => dispatch(searchRepositories(search));
+
     return (
     <SearchBar
         placeholder="Busca por repositórios"
         onChangeText={searchFilterFunction}
         autoCorrect={false}
+        onKeyboardHide={fetchRepositories}
+        onSubmitEditing={fetchRepositories}
         value={search}
         lightTheme
         round
--- src/redux/actions/repo.js
+++ src/redux/actions/repo.js
@@ -7,7 +7,7 @@ import { GET_REPOSITORIES } from '../../constants';
 export const getRepositories = () => {
     try {
       return async dispatch => {
-        const response = await axios.get(`https://api.github.com/search/repositories?q=TERMO_DA_BUSCA&per_page=10`);
+        const response = await axios.get(`https://api.github.com/search/repositories?q=TERMO_DA_BUSCA&per_page=20`);
         if (response.data) {
           dispatch({
             type: GET_REPOSITORIES,
@@ -21,4 +21,23 @@ export const getRepositories = () => {
       // Add custom logic to handle errors
       console.log(error);
     }
-  };
+};
+
+export const searchRepositories = term => {
+  try {
+    return async dispatch => {
+      const response = await axios.get(`https://api.github.com/search/repositories?q=${term}&per_page=20`);
+      if (response.data) {
+        dispatch({
+          type: GET_REPOSITORIES,
+          payload: response.data.items
+        });
+      } else {
+        console.log('Unable to fetch data from the API BASE URL!');
+      }
+    };
+  } catch (error) {
+    // Add custom logic to handle errors
+    console.log(error);
+  }
+};
